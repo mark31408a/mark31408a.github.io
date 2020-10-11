3c0c4fee655c60f3767b898012410f69977699aa
chungyao <chungyao@chungyaodeMacBook-Pro.local>
2020-10-11 16:35:22 +0800

    Updates

---
+++ precache-manifest.5b86c18e0011fe6f6d20efb0d279e570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7278a60500930b59f74e2f08cc97b38d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0176373af2835d0ad29b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "34f4569f477b42c6d931",
+    "url": "/static/js/2.ed3e592e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ed3e592e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0176373af2835d0ad29b",
+    "url": "/static/js/main.97d62480.chunk.js"
+  },
+  {
+    "revision": "183db94de12628515374",
+    "url": "/static/js/runtime-main.f0911912.js"
+  }
+]);
